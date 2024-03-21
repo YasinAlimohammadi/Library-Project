5b5c12aa01d66ea576b9cc4cdc19e3e62ead8d8d
Yasin AliMohammadi <tscosport@gmail.com>
2024-03-21 08:08:58 +0330

    Edit and add another files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Layout from "./layout/Layout";
+
 function App() {
   return (
     <>
-      <h1>Book App Starting files</h1>
+      <Layout>
+        <h1>Book-App starting files</h1>
+      </Layout>
     </>
   );
 }
